7d3ecc37765d92fe74c8a98806c5c9656457bdd1
Arthur Kozhemyakin <trable228@icloud.com>
2023-05-24 02:13:57 +0300

    added toasts on clicks, ripple effect for buttons

--- app/src/main/java/com/landfathich/dreamfilm/MainActivity.kt
+++ app/src/main/java/com/landfathich/dreamfilm/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.landfathich.dreamfilm
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.landfathich.dreamfilm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnMenu.setOnClickListener {
+            Toast.makeText(this, "Menu", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnFavorites.setOnClickListener {
+            Toast.makeText(this, "Favorites", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnSelections.setOnClickListener {
+            Toast.makeText(this, "Selections", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnWatchLater.setOnClickListener {
+            Toast.makeText(this, "Watch later", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnSettings.setOnClickListener {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
